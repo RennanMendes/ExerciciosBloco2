cb2f8d02acce1565b1676c81b689fe0e485abb31
rennan abreu <amrennan@gmail.com>
2022-01-24 17:21:26 -0300

    Add. Loja de Games

---
+++ Spring Boot/LojaDeGames/LojaDeGames/src/main/java/com/LojaDeGames/LojaDeGames/model/Produto.java
@@ -0,0 +1,77 @@
+package com.LojaDeGames.LojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "É obrigatorio informar uma categoria!") 
+	@Size(min = 2, max = 100, message = "O atributo categoria deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	private int quantidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("Produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+}
